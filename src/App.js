ec33cadaec6023a8b4da2e9405595afe7b2e9e2a
Abhav Thakur <abhav.thakur>
2023-11-25 17:44:14 +0530

    basic route setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
-      <h1 className="2-xl">hellow qorld</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<div>About</div>} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
